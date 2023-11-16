9d001ee5af67352eade65d28b230c6bf7fb27b2f
kimsoyol <naywinlhaing@gmail.com>
2023-11-16 17:26:49 +0630

    fix events cards

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,10 +1,7 @@
 import { useSelector } from "react-redux";
 
 const Events = () => {
-
-  
   const events = useSelector((state) => state.event.events);
-
 
   return (
     <section className="px-20 py-32">
@@ -14,17 +11,27 @@ const Events = () => {
           View All Exhibitions →
         </button>
       </div>
-      <div className="flex gap-5 pt-20 justify-evenly flex-wrap">
+      <div className="flex gap-5 pt-20 justify-evenly flex-wrap text-Brown">
         {events.map((event) => (
-          <div key={event.id} className="border-solid border-[1.5px] border-Brown p-2 h-[624px]">
+          <div
+            key={event.id}
+            className="border-solid border-[1.5px] border-[#883B0A29] p-2 h-[624px]"
+          >
             <img
-              className="w-[421px] h-[421px]"
+              className="w-[421px] h-[421px] pb-8"
               src={event.img}
               alt="event poster"
             />
-            <p>{event.title}</p>
-            <p>{event.date}</p>
-            <p>{event.location}</p>
+            <p className="pb-8 font-semibold text-2xl pl-3">{event.title}</p>
+            <div className="flex justify-between p-3">
+              <div>
+                <p className="">{event.date}</p>
+                <p>{event.location}</p>
+              </div>
+              <button className="border-solid border-[1.5px] border-Brown py-3 px-7">
+                More Details
+              </button>
+            </div>
           </div>
         ))}
       </div>
